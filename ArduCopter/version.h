e4ae00e53e766f51b9d2d806f5b08cda86418ed7
a.skakun <a.skakun@steel.mil.ua>
2025-02-20 17:09:41 +0200

    OSD num sats support 3 symbols, should be less 255 VO will send 100+sats

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.6.2 - v180225"
+#define THISFIRMWARE "ArduCopter V4.6.2 - v210225"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,6,2,FIRMWARE_VERSION_TYPE_OFFICIAL
