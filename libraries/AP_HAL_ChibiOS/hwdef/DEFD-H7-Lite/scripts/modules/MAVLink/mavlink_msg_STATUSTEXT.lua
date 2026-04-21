f606d59b3e6591780436a2cf2c7159b4714d5bba
Mykhailo Tomashivskyi <m.tomashivskyi@steel.mil.ua>
2026-04-21 12:03:13 +0300

    configuration for new flight controller

---
+++ libraries/AP_HAL_ChibiOS/hwdef/DEFD-H7-Lite/scripts/modules/MAVLink/mavlink_msg_STATUSTEXT.lua
@@ -0,0 +1,9 @@
+local STATUSTEXT = {}
+STATUSTEXT.id = 253
+STATUSTEXT.fields = {
+            { "severity", "<B" },
+            { "text", "<c50" },
+            { "id", "<I2" },
+            { "chunk_seq", "<B" },
+         }
+return STATUSTEXT
