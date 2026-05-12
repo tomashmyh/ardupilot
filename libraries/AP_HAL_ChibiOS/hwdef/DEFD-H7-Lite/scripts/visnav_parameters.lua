9b8fa9e84c93b0b020e74ce55fb48a07489d13f3
Mykhailo Tomashivskyi <m.tomashivskyi@steel.mil.ua>
2026-05-12 14:15:37 +0300

    include visnav parameters inside a defdrones build

---
+++ libraries/AP_HAL_ChibiOS/hwdef/DEFD-H7-Lite/scripts/visnav_parameters.lua
@@ -0,0 +1,13 @@
+-- Adding parameters of Visual Navigation via lua script
+
+-- The key is persistent in storage and must be unique. Value 0..200.
+local PARAM_TABLE_KEY = 158
+
+-- Create a parameter table
+assert(param:add_table(PARAM_TABLE_KEY, "VISNAV_", 2), 'could not add param table')
+
+-- Create parameters.
+-- The param indexes (2nd argument) must be between 1 and 63.
+-- All added parameters are floats, with the given default value (4th argument).
+assert(param:add_param(PARAM_TABLE_KEY, 1, 'CTRL_CH', 10.0), 'could not add CTRL_CH')
+assert(param:add_param(PARAM_TABLE_KEY, 2, 'CAM_ANGLE', 90.0), 'could not add CAM_ANGLE')
