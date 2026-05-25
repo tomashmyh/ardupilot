031d6f9759f472f0b0d390f21dde055e2d912eba
Mykhailo Tomashivskyi <m.tomashivskyi@steel.mil.ua>
2026-05-25 15:53:03 +0300

    enable r2d2 sync by default

--- libraries/AP_HAL_ChibiOS/hwdef/DEFD-H7-Lite/scripts/visnav_parameters.lua
+++ libraries/AP_HAL_ChibiOS/hwdef/DEFD-H7-Lite/scripts/visnav_parameters.lua
@@ -11,6 +11,6 @@ assert(param:add_table(PARAM_TABLE_KEY, "VISNAV_", 5), 'could not add param tabl
 -- All added parameters are floats, with the given default value (4th argument).
 assert(param:add_param(PARAM_TABLE_KEY, 1, 'CTRL_CH', 10.0), 'could not add CTRL_CH')
 assert(param:add_param(PARAM_TABLE_KEY, 2, 'CAM_ANGLE', 90.0), 'could not add CAM_ANGLE')
-assert(param:add_param(PARAM_TABLE_KEY, 3, 'R2D2_SYNC', 0), 'could not add R2D2_SYNC')
+assert(param:add_param(PARAM_TABLE_KEY, 3, 'R2D2_SYNC', 1), 'could not add R2D2_SYNC')
 assert(param:add_param(PARAM_TABLE_KEY, 4, 'GPS_SYNC', 1), 'could not add GPS_SYNC')
 assert(param:add_param(PARAM_TABLE_KEY, 5, 'SNLN_SYNC', 0), 'could not add SNLN_SYNC')
