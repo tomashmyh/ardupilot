aa7ab7a0f05be2191b4a8b2550fa24a2957c3f83
Anton Horenkov <a.horenkov@steel.mil.ua>
2026-07-13 17:23:55 +0300

    Added VISNAV_HOME_SYNC param

--- libraries/AP_HAL_ChibiOS/hwdef/MatekH743-bdshot/scripts/visnav_parameters.lua
+++ libraries/AP_HAL_ChibiOS/hwdef/MatekH743-bdshot/scripts/visnav_parameters.lua
@@ -4,7 +4,7 @@
 local PARAM_TABLE_KEY = 158
 
 -- Create a parameter table
-assert(param:add_table(PARAM_TABLE_KEY, "VISNAV_", 5), 'could not add param table')
+assert(param:add_table(PARAM_TABLE_KEY, "VISNAV_", 6), 'could not add param table')
 
 -- Create parameters.
 -- The param indexes (2nd argument) must be between 1 and 63.
@@ -13,4 +13,5 @@ assert(param:add_param(PARAM_TABLE_KEY, 1, 'CTRL_CH', 10.0), 'could not add CTRL
 assert(param:add_param(PARAM_TABLE_KEY, 2, 'CAM_ANGLE', 90.0), 'could not add CAM_ANGLE')
 assert(param:add_param(PARAM_TABLE_KEY, 3, 'R2D2_SYNC', 0), 'could not add R2D2_SYNC')
 assert(param:add_param(PARAM_TABLE_KEY, 4, 'GPS_SYNC', 1), 'could not add GPS_SYNC')
-assert(param:add_param(PARAM_TABLE_KEY, 5, 'SNLN_SYNC', 0), 'could not add SNLN_SYNC')
+assert(param:add_param(PARAM_TABLE_KEY, 5, 'GCS_SYNC', 1), 'could not add GCS_SYNC')
+assert(param:add_param(PARAM_TABLE_KEY, 6, 'SNLN_SYNC', 0), 'could not add SNLN_SYNC')
